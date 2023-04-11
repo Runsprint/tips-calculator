c4c08ced72feac70d2cd5e04e991383ba0c83fad
gvantsa kapanadze <gvantsakapanadze95@gmail.com>
2023-04-11 14:18:33 +0400

    last

--- index.js
+++ index.js
@@ -1,132 +1,103 @@
-let firstInput = document.getElementById("bill_input");
-let five = document.getElementById("five");
-let fifteen = document.getElementById("fifteen");
-let fiftee = document.getElementById("fiftee");
-let ten = document.getElementById("ten");
-let twentyfive = document.getElementById("twentyfive");
-let custom = document.getElementById("custom");
-let people_input = document.getElementById("people_input");
-let output1 = document.getElementById("output1"); //value
-let output2 = document.getElementById("output2");
-let reset = document.getElementById("reset");
-let cant = document.getElementById("cant");
-//this isButtonClicked is for  if one button is clicked, u cant clicked and write some value into "custome" button
-// let isButtonClicked = false;
-//and I used custom.disabled = true; cuz I wanted when I clicked any button , in custom I cant write some value
+'use strict' //its for errors
+ let firstInput = document.getElementById("bill_input");
+ let people_input = document.getElementById("people_input");
+ let output1 = document.getElementById("output1");
+ let output2 = document.getElementById("output2");
+ let reset = document.getElementById("reset");
+ let cant = document.getElementById("cant");
+ let buttons = document.querySelectorAll(".percent");
+ let custom = document.getElementById("custom")
+ let active = null;
+//I usec forech for arrays and parseFloat for take number drom string(there is second variant too) and toFixed, cuz I wanted fix numbers fetr whole numbers.
+//I used min and max in html and oninput="validity.valid||(value='');" cus I wanted write just positive numbers
+// I used element active process where active is null from the beginning and after one click it not null and have some meaning and 
+//the first if is when oldeady have clicked and clicked second time another or same button. and second if is for the first click where active take meaning.
+// after, in second if I wrote what I wanted to happaned after first click.after irst click active which was null = element.and after every manipulation happan on element and not on active
+buttons.forEach(element => {
+    element.addEventListener('click', ()=>{
+        if(firstInput.value !== 0 && people_input.value !== 0 ){
+            let tip =(parseFloat(element.textContent)/100) * firstInput.value ;
+            output1.textContent = (tip/people_input.value).toFixed(2); 
+            let variable = Number(firstInput.value)+tip;
+            output2.textContent = (variable / people_input.value).toFixed(2);
+        }
+        if(element === active){
+            element.style.backgroundColor = "#00474B"; 
+            active = null;
+          }else {
+           element.style.backgroundColor = "#26C2AE";
+    
+           if(active){
+           active.style.backgroundColor = "#00474B"; 
+          }
+          active = element;
+          }
+          
+        changes();
+        reset.style.backgroundColor = "#26C2AE";    
+    }) 
+});
 function changes() {
     if(firstInput.value !== 0 && people_input.value == 0){
         cant.style.display = "flex"
         output1.textContent = " $00.00";
         output2.textContent = "$00.00";
-        people_input.style.borderColor = "red";
+        firstInput.style.borderColor = "#d9d9d9";
     }else {
         cant.style.display = "none";
         people_input.style.borderColor = "#d9d9d9";
-    } 
-    
+        firstInput.style.borderColor = "#d9d9d9";
+    }
+    if(firstInput.value == 0 && people_input.value !== 0){
+        firstInput.style.borderColor = "red";
+    }
+    if(firstInput.value == 0 && people_input.value == 0){
+        firstInput.style.borderColor = "red";
+        cant.style.display = "flex";
+        people_input.style.borderColor = "red";   
+    }
 }
-// i used oninput="validity.valid||(value='');"< for input values be just possitive numbers
-five.addEventListener("click", () =>{
-    if(firstInput.value !== 0 && people_input.value !== 0){ 
-        let tip = (firstInput.value * 0.05);
-        output1.textContent = (tip / people_input.value).toFixed(2); 
-        let variable = firstInput.value +++ tip
-        output2.textContent =  (variable / people_input.value).toFixed(2);
-        five.style.backgroundColor = "#26C2AE";
-        fifteen.style.backgroundColor = "#00474B";
-        fiftee.style.backgroundColor = "#00474B";
-        twentyfive.style.backgroundColor = "#00474B";
-        ten.style.backgroundColor = "#00474B";
-        custom.style.borderColor = "#547878"; 
-        reset.style.backgroundColor = "#26C2AE";  
-    };
-    changes();
-    custom.disabled = true;
-})
-fifteen.addEventListener("click", () =>{
-    if(firstInput.value !== 0 && people_input.value !== 0){
-       let tip = (firstInput.value * 0.15);
-       output1.textContent = (tip / people_input.value).toFixed(2); 
-       let variable = firstInput.value +++ tip
-       output2.textContent = (variable / people_input.value).toFixed(2); 
-       fifteen.style.backgroundColor = "#26C2AE";
-       five.style.backgroundColor = "#00474B";
-       fiftee.style.backgroundColor = "#00474B";
-       twentyfive.style.backgroundColor = "#00474B";
-       ten.style.backgroundColor = "#00474B";
-       custom.style.borderColor = "#547878";
-       reset.style.backgroundColor = "#26C2AE";
-    } 
-    changes();
-    custom.disabled = true;
-})
-fiftee.addEventListener("click", () =>{
-    if(firstInput.value !== 0 && people_input.value !== 0){
-        let tip = (firstInput.value * 0.5);
-        output1.textContent = (tip / people_input.value).toFixed(2); 
-        let variable = firstInput.value +++ tip
-        output2.textContent = (variable / people_input.value).toFixed(2);
-        fiftee.style.backgroundColor = "#26C2AE";
-        fifteen.style.backgroundColor = "#00474B";
-        five.style.backgroundColor = "#00474B";
-        twentyfive.style.backgroundColor = "#00474B";
-        ten.style.backgroundColor = "#00474B";
-        custom.style.borderColor = "#547878"; 
-        reset.style.backgroundColor = "#26C2AE"; 
-     }  
-     changes();
-     custom.disabled = true;
-})
-ten.addEventListener("click", () =>{
-    if(firstInput.value !== 0 && people_input.value !== 0){
-        let tip = (firstInput.value * 0.1);
-        output1.textContent = (tip / people_input.value).toFixed(2); 
-        let variable = firstInput.value +++ tip
-        output2.textContent = (variable / people_input.value).toFixed(2);  
-        ten.style.backgroundColor = "#26C2AE"; 
-        fiftee.style.backgroundColor = "#00474B";
-        fifteen.style.backgroundColor = "#00474B";
-        five.style.backgroundColor = "#00474B";
-        twentyfive.style.backgroundColor = "#00474B";
-        custom.style.borderColor = "#547878";
-        reset.style.backgroundColor = "#26C2AE";
-     } 
-     changes();
-     custom.disabled = true;
-})
-twentyfive.addEventListener("click", () =>{
-    if(firstInput.value !== 0 && people_input.value !== 0){
-        let tip = (firstInput.value * 0.25);
-        output1.textContent = (tip / people_input.value).toFixed(2); 
-        let variable = firstInput.value +++ tip
-        output2.textContent = (variable / people_input.value).toFixed(2); 
-        twentyfive.style.backgroundColor = "#26C2AE"; 
-        ten.style.backgroundColor = "#00474B"; 
-        fiftee.style.backgroundColor = "#00474B";
-        fifteen.style.backgroundColor = "#00474B";
-        five.style.backgroundColor = "#00474B";
-        custom.style.borderColor = "#547878";
-        reset.style.backgroundColor = "#26C2AE";
-     } 
-    changes();
-    custom.disabled = true;
-})
 custom.addEventListener("input", () =>{
     if(firstInput.value !== 0 && people_input.value !== 0 ){
         let tip = ((custom.value / 100)*firstInput.value);
         output1.textContent = (tip/people_input.value).toFixed(2); 
-        let variable = firstInput.value +++ tip;
-        output2.textContent = (variable / people_input.value).toFixed(2);  
-        custom.style.borderColor = "#9FE8DF";
-        twentyfive.style.backgroundColor = "#00474B"; 
-        ten.style.backgroundColor = "#00474B"; 
-        fiftee.style.backgroundColor = "#00474B";
-        fifteen.style.backgroundColor = "#00474B";
-        five.style.backgroundColor = "#00474B";
-        reset.style.backgroundColor = "#26C2AE";
-     } 
-    custom();   
+        let variable = Number(firstInput.value)+tip;
+        output2.textContent = (variable / people_input.value).toFixed(2);
+    }
+    buttons.forEach(function(element){
+        element.style.backgroundColor = "#00474B"
+    })
+    changes(); 
+    custom.style.borderColor = "#9FE8DF";
+    reset.style.backgroundColor = "#26C2AE";   
 })
+
 reset.addEventListener("click", ()=>{
     location.reload();
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
